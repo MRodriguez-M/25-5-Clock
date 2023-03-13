373aad5c4f794fe63c545135df1957469f6df22e
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-03-13 12:53:05 -0600

    Set up container and label

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-const Timer = () => {
+const Timer = (props) => {
     return (
+        <div id="timer-container">
+            <p id="timer-label">{props.label}</p>
+        </div>
     );
 };
 
