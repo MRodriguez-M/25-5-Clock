702ae3b1cf8f2e24cc0a89aa7cafe2cffdebe5e0
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-04-14 15:55:37 -0600

    Add onClick for buttons

--- src/components/AdjustButtons.js
+++ src/components/AdjustButtons.js
@@ -4,9 +4,9 @@ const AdjustButtons = (props) => {
     return (
         <div id="container">
             <p id={`${props.label}-label`}>{props.label} Length</p>
-            <button id={`${props.label}-decrement`}>-</button>
+            <button id={`${props.label}-decrement`} onClick={props.handleClickDecrement}>-</button>
             <p id={`${props.label}-length`}>{props.length}</p>
-            <button id={`${props.label}-increment`}>+</button>
+            <button id={`${props.label}-increment`} onClick={props.handleClickIncrement}>+</button>
         </div>
     );
 };
